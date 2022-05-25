3c90c30269ac899e1a7f0b383e11843fb24d11fa
Andrin Meier <andrin@pricemeier.ch>
2022-05-25 09:40:06 +0200

    Add SFTP script for directly deploying to hoststar including a manually triggered workflow

---
+++ deploy_site.py
@@ -0,0 +1,58 @@
+import os
+from typing import List
+import paramiko
+
+class MySFTPClient(paramiko.SFTPClient):
+    def put_dir(self, source, target, excluded_dirs: List[str] = []):
+        for item in os.listdir(source):
+            if item in excluded_dirs:
+                print(f"Skipping {item} file/folder.")
+                continue
+            if os.path.isfile(os.path.join(source, item)):
+                self.put(os.path.join(source, item), '%s/%s' % (target, item))
+            else:
+                self.mkdir('%s/%s' % (target, item), ignore_existing=True)
+                self.put_dir(os.path.join(source, item), '%s/%s' % (target, item))
+
+def get_env_str(key) -> str:
+    if key not in os.environ:
+        return ""
+    return os.environ[key]
+
+def get_env_int(key) -> str:
+    if key not in os.environ:
+        return -1
+    value = os.environ[key]
+    if not value.isdigt():
+        raise Exception(f"Expected ENV.{key} to be a number.")
+    return int(value)
+
+def get_env_str_array(key, split_by = ',') -> str:
+    if key not in os.environ:
+        return []
+    value = os.environ[key]
+    return value.split(split_by)
+
+
+sftp = None
+try:
+    local_path = get_env_str("DEPLOYMENT_LOCAL_PATH")
+    remote_path = get_env_str("DEPLOYMENT_REMOTE_PATH")
+    HOST = get_env_str("DEPLOYMENT_HOST")
+    PORT = get_env_int("DEPLOYMENT_PORT")
+    USERNAME = get_env_str("DEPLOYMENT_USERNAME")
+    PASSWORD = get_env_str("DEPLOYMENT_PASSWORD")
+    excluded_dirs = get_env_str_array("DEPLOYMENT_EXCLUDED_DIRS")
+    script_name = os.path.basename(__file__)
+    excluded_dirs.append(script_name)
+    transport = paramiko.Transport((HOST, PORT))
+    transport.connect(username=USERNAME, password=PASSWORD)
+    sftp = MySFTPClient.from_transport(transport)
+    sftp.put_dir(local_path, remote_path, excluded_dirs=excluded_dirs)
+    sftp.close()
+except Exception as ex:
+    print("Deployment failed.")
+    print(repr(ex))
+finally:
+    if sftp is not None:
+        sftp.close()
